2a452d7f35818df155678a506796e3cd0626680c
gpossas <possasg@gmail.com>
2025-11-20 16:38:11 -0300

    feat: add action to logo to go back to home while in watchlist page

--- src/routes/watchlist/+page.svelte
+++ src/routes/watchlist/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+    import { goto } from '$app/navigation';
     import MovieCard from '$lib/components/MovieCard.svelte';
     import { watchlist } from '../../stores/watchlist';
+
+    function goToHome() {
+        goto('/');
+    }
 </script>
+
+<button class="center" onclick={goToHome}>
+    <img src="rotten_tomatoes_logo.png" alt="Rotten Tomatoes" class="logo" id="logo" />
+</button>
 
 <h1>Your Watchlist</h1>
 
@@ -13,10 +22,23 @@
 
 
 <style>
+    #logo {
+        height: 40px;
+
+    }
     .grid {
         display: grid;
         grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
         gap: 22px;
         padding: 16px;
+    }
+
+    button {
+        background: none;
+        border: none;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 12px;
     }
 </style>
