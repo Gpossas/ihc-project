b70f96a3c41397476d6c16fcbd736eddc4829624
MauricioSiqueira <mauricegdev@gmail.com>
2025-11-24 00:51:19 -0300

    ajuste no width da bar

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
+  import { goto } from "$app/navigation";
 
   let query = "";
 
@@ -11,17 +11,16 @@
 </script>
 
 <div class="search-bar">
-    <img class="icon" src="search_icon.png" alt="">
-    
-    <input 
-      tabindex="1"
-      type="text" 
-      placeholder="Buscar..."
-      bind:value={query}
-      on:keydown={handleKey}
-    />
-</div>
+  <img class="icon" src="search_icon.png" alt="" />
 
+  <input
+    tabindex="1"
+    type="text"
+    placeholder="Buscar..."
+    bind:value={query}
+    on:keydown={handleKey}
+  />
+</div>
 
 <style>
   .search-bar {
@@ -30,30 +29,29 @@
     background: #bfbfbf;
     border-radius: 40px;
     padding: 11px 16px;
-    width: 80%;
-     
+    width: 60%;
+
     border-radius: 40px;
 
     display: flex;
     align-items: center;
     gap: 10px;
 
-    box-shadow: 0 4px 10px rgba(0,0,0,0.2);
-    
-}
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  }
 
-.search-bar .icon {
+  .search-bar .icon {
     height: 20px;
-}
+  }
 
-.search-bar input {
+  .search-bar input {
     flex: 1;
     border: none;
     outline: none;
     background: transparent;
     font-size: 20px;
     color: #333;
-}
+  }
 
   input:focus {
     border-color: #888;
